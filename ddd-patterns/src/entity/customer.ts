dcb12757461e5edb08fda140f5fdcff612b9c5b9
Henrique Degrecci <hdegrecci@gmail.com>
2024-05-04 20:42:42 -0300

    ddd-pattern: business rules

--- ddd-patterns/src/entity/customer.ts
+++ ddd-patterns/src/entity/customer.ts
@@ -2,34 +2,28 @@ class Customer {
   _id: string;
   _name: string;
   _address: string;
+  _active: boolean;
 
   constructor(id: string, name: string, address: string) {
     this._id = id;
     this._name = name;
     this._address = address;
+    this._active = true;
   }
 
-  get id(): string {
-    return this._id;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  get address(): string {
-    return this._address;
-  }
-
-  set id(id: string) {
-    this._id = id;
-  }
-
-  set name(name: string) {
+  set name(name: string) {\
     this._name = name;
   }
 
-  set address(address: string) {
-    this._address = address;
+  changeName(name: string) {
+    this.name = name;
+  }
+
+  activate() {
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
   }
 }
