aacf1cddd135f0eb47f744d582185fbead4c01bb
Henrique Degrecci <hdegrecci@gmail.com>
2024-05-07 22:11:12 -0300

    ddd-patterns: end product class and tests

--- ddd-patterns/src/entity/product.ts
+++ ddd-patterns/src/entity/product.ts
@@ -11,9 +11,34 @@ export default class Product {
     this.validate();
   }
 
+  get name(): string {
+    return this._name;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  changeName(name: string): void {
+    this._name = name;
+    this.validate();
+  }
+
+  changePrice(price: number): void {
+    this._price = price;
+    this.validate();
+  }
+
   validate(): boolean {
     if (!this._id) {
       throw new Error("Id is required");
+    }
+    if (!this._name) {
+      throw new Error("Name is required");
+    }
+
+    if (this._price < 0) {
+      throw new Error("Price is less than 0");
     }
     return true;
   }
