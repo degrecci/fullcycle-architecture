64104e349695cfde9ddc548a85140c5dc9c0abcd
Henrique Degrecci <hdegrecci@gmail.com>
2024-05-29 23:20:11 -0300

    ddd-patterns: create event and event handler

--- ddd-patterns/src/domain/event/@shared/event-dispatcher.spec.ts
+++ ddd-patterns/src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,3 +1,6 @@
+import EventDispatcher from "./event-dispatcher";
+import SendEmailWhenProductIsCreatedHandler from "./product/handler/send-email-when-product-is-created.handler";
+
 describe("Domain events tests", () => {
   it("should register an event handler", () => {
     const eventDispatcher = new EventDispatcher();
